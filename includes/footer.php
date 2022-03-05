019539e522e61ffadc504fec8dbbcded629ac14f
Jeevika <jikrparab@gmail.com>
2022-03-05 20:02:55 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<div class="container">
+      <centre>
+		<p>Copyright <span class="glyphicon glyphicon-copyright-mark"></span> LifeStyle Stores.
+		All Rights Reserved | Contact Us: +91 90000 00000 </p>
+     </centre>
+	</div>
+</footer>
